7711458155c2c5c3ee6414f6510d5217a89b5975
SRC0747 <RjSrc@0747>
2021-10-22 01:52:07 +0530

    Welcome to 4 Balls Challenging game using OOPS Concept.

---
+++ src/test/java/com/fourballchallenge/FourBallChallengeTest.java
@@ -0,0 +1,13 @@
+package com.fourballchallenge;
+
+import static org.junit.jupiter.api.Assertions.*;
+import processing.core.PApplet;
+
+class FourBallChallengeTest {
+    public static void main(String[] args) {
+        PApplet.main("FourBallChallenge", args);
+
+        FourBallChallenge fourBallChallenge = new FourBallChallenge();
+        //fourBallChallenge.paintWhite();
+    }
+}
